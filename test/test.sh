f95d1ef1c4d572b6fc60d2ce5c4956a0b6315370
Taylor Monacelli <taylormonacelli@gmail.com>
2023-01-24 09:26:49 -0800

    Add e2e test

---
+++ test/test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+set -x
+set -e
+set -u
+
+kubectl create namespace finance
+kubectl create deployment nginx --image nginx --namespace finance
+kubectl rollout status deployment/nginx --namespace finance
+kubectl create role myuser-finance --verb get,list --resource=deployments --namespace finance
+kubectl create rolebinding finance-rolebinding --role myuser-finance --user myuser --namespace finance
+
+timeout --verbose 1m \
+    ./dist/k8suser_linux_amd64 \
+    --orgUnit DevOps \
+    --username myuser \
+    --email 'myuser@gmail.com' \
+    --country US \
+    --city IL \
+    --orgazniation acmecorp \
+    --province Chicago \
+    --cluster kind-kind
+
+kubectl --kubeconfig ./myuser get deployment nginx --namespace finance
